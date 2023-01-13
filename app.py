57b0504c962e85582166442282e6645fc3d08c95
Vitalii Mahdych <vitalii.mahdych@gmail.com>
2023-01-13 10:46:48 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    now = datetime.now()
+    return render_template('index.html', year=now.year)
+
+if __name__ == '__main__':
+    app.run(debug=True)
